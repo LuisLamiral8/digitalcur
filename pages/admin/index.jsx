7951f2157abee3076193a86b1986efe75034fef5
LuisLamiral8 <luislamiraal@gmail.com>
2022-04-26 19:33:53 -0300

    changed select amount on product page

---
+++ pages/admin/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../../components/Layout";
+const index = () => {
+  return (
+    <Layout
+      title="Home | DigitalCur"
+      desc="DigitalCur"
+      icon1="/icons/gamesphysical.svg"
+      icon2="/icons/gamesdigital.svg"
+      icon3="/icons/games.svg"
+      icon4="/icons/gamesstreaming.svg"
+      class1="item"
+      class2="item"
+      class3="item"
+      class4="item"
+    >
+      <h1>hola</h1>
+    </Layout>
+  );
+};
+
+export default index;
